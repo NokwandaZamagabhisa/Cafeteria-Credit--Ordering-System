f5b8e37ca0fa57888bb64c536580188c986afa3a
NokwandaZamagabhisa <$$Dut970523>
2025-07-15 20:33:59 +0200

    Configure the one-to-many relationship

--- Ordering_System/Data/EmployeeDbContext.cs
+++ Ordering_System/Data/EmployeeDbContext.cs
@@ -11,5 +11,19 @@ namespace Ordering_System.Data
             
         }
         public virtual DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the one-to-many relationship
+            modelBuilder.Entity<MenuItem>()
+                .HasOne(m => m.Restaurant)  // A MenuItem has one Restaurant
+                .WithMany(r => r.MenuItems) // A Restaurant has many MenuItems
+                .HasForeignKey(m => m.RestaurantId); // Foreign key in MenuItem
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
