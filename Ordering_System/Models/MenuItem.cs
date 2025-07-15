726f0973be632fb1dd67348aa10881c6f5567cf9
NokwandaZamagabhisa <$$Dut970523>
2025-07-15 19:37:57 +0200

    Add Restaurant & MenuItem models

---
+++ Ordering_System/Models/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering_System.Models
+{
+    public class MenuItem
+    {
+        // Primary Key for MenuItem model
+        public int Id { get; set; }
+
+        // Foreign Key to the Restaurant model
+        public int RestaurantId { get; set; }
+
+        // Navigation property to the related Restaurant
+        public Restaurant Restaurant { get; set; } = "";
+
+        // Name of the menu item (e.g., "Big Mac Meal")
+        public string Name { get; set; } = "";
+
+        // Description of the menu item
+        public string Description { get; set; } = "";
+
+        // Price of the menu item
+        public decimal Price { get; set; }
+    }
+}
---
+++ Ordering_System/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering_System.Models
+{
+    public class Restaurant
+    {
+
+        // Primary Key for the Restaurant model
+        public int Id { get; set; }
+
+        // Name of the restaurant (e.g., "McDonald's Braamfontein")
+        public string Name { get; set; } = "";
+
+        // A brief description of the restaurant's location (e.g., "Jan Smuts Avenue")
+        public string LocationDescription { get; set; } = "";
+
+        // Contact number for the restaurant
+        public string ContactNumber { get; set; } = "";
+    }
+
+}
